078d0bfc291cd8ed05f7f480df3ccd7fcf6ce9d3
Gnanacardhan961 <gnanavardhanreddy06@gmail.com>
2025-10-08 21:23:22 +0530

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SearchResults from "./pages/SearchResults";
 import MyBookings from "./pages/MyBookings";
@@ -6,14 +6,14 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <Router>
+    <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/search" element={<SearchResults />} />
         <Route path="/my-bookings" element={<MyBookings />} />
       </Routes>
-    </Router>
+    </>
   );
 }
 
