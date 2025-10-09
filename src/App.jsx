aeff1bfc67f83344ba06d7a1371534b54e8fb061
Gnanacardhan961 <gnanavardhanreddy06@gmail.com>
2025-10-09 12:48:59 +0530

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
 // src/App.jsx
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import SearchResults from "./pages/SearchResults";
 import MyBookings from "./pages/MyBookings";
 
-
 const App = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/search" element={<SearchResults />} />
-      <Route path="/my-bookings" element={<MyBookings />} /> {/* Critical path */}
-    </Routes>
-  </Router>
+  <Routes>
+    <Route path="/" element={<LandingPage />} />
+    <Route path="/search" element={<SearchResults />} />
+    <Route path="/my-bookings" element={<MyBookings />} />
+  </Routes>
 );
 
 export default App;
