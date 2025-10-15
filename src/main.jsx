9b996b145244fd750f4d0391b0e7521b9d540bd0
Gnanacardhan961 <gnanavardhanreddy06@gmail.com>
2025-10-15 17:11:23 +0530

    Initial commit - Vite project

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App";
 import { HashRouter } from "react-router-dom"; // Use HashRouter for SPA routing
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
+  <Router>
     <App />
-  </HashRouter>
+  </Router>
 );
