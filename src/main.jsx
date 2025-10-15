d5c8ad7e242822236d6446fc488dcf5abf0851fb
Gnanacardhan961 <gnanavardhanreddy06@gmail.com>
2025-10-15 17:14:40 +0530

    Initial commit - Vite project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import {Router } from "react-router-dom"; 
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Router>
+  <BrowserRouter>
     <App />
-  </Router>
+  </BrowserRouter>
 );
