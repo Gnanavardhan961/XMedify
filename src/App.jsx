6ff7d8a69b54bca0eb600cab4d813eaa02a26c15
Gnanacardhan961 <gnanavardhanreddy06@gmail.com>
2025-10-16 23:44:32 +0530

    Booking

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Home from "./Home/Home";
 import Search from "./Search/Search";
@@ -10,13 +10,13 @@ import "./App.css";
 
 export default function App() {
   return (
-    <Router>
+    <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/search" element={<Search />} />
         <Route path="/my-bookings" element={<MyBookings />} />
       </Routes>
-    </Router>
+    </>
   );
 }
